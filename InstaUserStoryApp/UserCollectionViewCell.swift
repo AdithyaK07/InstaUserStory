f2f110e36e344c479c5d536f7ab251b976cd01ad
Adithya <adithya.kesarla@gmail.com>
2017-11-01 19:42:17 +0530

    Basic UI for the First Screen

---
+++ InstaUserStoryApp/UserCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  UserCollectionViewCell.swift
+//  InstaUserStoryApp
+//
+//  Created by Adithya on 11/1/17.
+//  Copyright © 2017 Adithya. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+}
